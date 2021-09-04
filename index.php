7d369bbcf5e7a2f2c5ca048f424b6cd8c382c3d0
Sexettin <72075007+sexettin78@users.noreply.github.com>
2021-09-04 21:18:16 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+ <script>
+       function yonlendir(){ 
+         window.location.href="https://ipchat.rf.gd";
+       }
+window.onload=yonlendir;       
+  </script>
+
+//SÜRELİ YÖNLENDİRME
+
+     <script>
+    setTimeout(function(){   
+        window.location.assign("https://ipchat.rf.gd");
+        //12 saniye sonra yönlenecek
+        }, 12000);
+  </script>
+
+
+
+
+
+
+
+//kaynak yazilimbilisim.net
+
+
